6fc9c0fa0941b0e3b63547158f5a2a5d713be7cb
Cydewyn <cydewyn@gmail.com>
2026-09-11 15:46:10 -0400

    Didn't delete - just added

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
 
   return (
     <>
+
+      <h1>HELLO WORLD 1</h1>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+function Main() {
+  return (
+    <>
+      <h1>Hello World 2</h1>
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main/>
   </StrictMode>,
 )
